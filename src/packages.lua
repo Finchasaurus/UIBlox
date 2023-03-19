09a0310908c749ccdd68aaeeb43d3e327905949c
ScriptingOtaku <0822djs@gmail.com>
2023-03-19 16:17:07 -0600

    packages

--- src/packages.lua
+++ src/packages.lua
@@ -4,8 +4,18 @@ local PACKAGE_ROOT_NAME = "lib";
 
 local packagesRoot = ReplicatedStorage:WaitForChild(PACKAGE_ROOT_NAME);
 
-local function packages(packageName: string) 
-    return packagesRoot:FindFirstChild(packageName, true);
+local map = {}
+
+local function packages(packageName: string)
+    if map[packageName] == nil then
+        local found = packagesRoot:FindFirstChild(packageName, true);
+        if found:IsA("ModuleScript") then 
+            return found 
+        else 
+            return found:FindFirstChild(packageName, true); 
+        end
+    end
+    return map[packageName];
 end
 
 return packages;
